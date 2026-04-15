379c14b6ea028e5c4e231c83108c01a3302d9d07
sahilgk-cms <sahil@catalysts.org>
2026-04-15 10:37:05 +0530

    data files

---
+++ data/schema.py
@@ -0,0 +1,8 @@
+DATE_COL_WEEK_START = "week_start"
+TARGET_COL = "no_of_cases_next_week"
+DISTRICT_COL = "dist_name"
+SUB_DISTRICT_COL = "sub_district"
+TEST_META_COLS = [DATE_COL_WEEK_START, 
+                  SUB_DISTRICT_COL, 
+                  #DISTRICT_COL, 
+                  TARGET_COL]
